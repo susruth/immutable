80e652053889f197912cd77105d193052b2a7f06
susruth <susruth@renproject.io>
2019-08-15 01:21:23 +1000

    add root level initializers

--- immutable.go
+++ immutable.go
@@ -1,5 +1,12 @@
 package immutable
 
-import "github.com/susruth/immutable/list"
+import (
+	"github.com/susruth/immutable/list"
+	"github.com/susruth/immutable/record"
+)
 
 type List = list.List
+type Record = record.Record
+
+var NewList = list.New
+var NewRecord = record.New
