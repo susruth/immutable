165cac9eef33e7e0e11469fa7521a32d81563581
susruth <susruth@renproject.io>
2019-08-14 22:33:44 +1000

    Add Immutable List

---
+++ immutable.go
@@ -0,0 +1,5 @@
+package immutable
+
+import "github.com/susruth/immutable/list"
+
+type List = list.List
---
+++ list/list_test.go
@@ -0,0 +1,63 @@
+package list_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/susruth/immutable/list"
+)
+
+var _ = Describe("Immutable List", func() {
+	Context("when appending elements to a list", func() {
+		It("should create new versions of the list when using ints", func() {
+			list := New(1, 2, 3, 4)
+			Expect(list.Values(0)).Should(Equal([]interface{}{1, 2, 3, 4}))
+
+			list.Append(5)
+			Expect(list.Values(1)).Should(Equal([]interface{}{1, 2, 3, 4, 5}))
+		})
+
+		It("should create new versions of the list when using strings", func() {
+			list := New("hi", "hello", "bye")
+			Expect(list.Values(0)).Should(Equal([]interface{}{"hi", "hello", "bye"}))
+
+			list.Append("good")
+			Expect(list.Values(1)).Should(Equal([]interface{}{"hi", "hello", "bye", "good"}))
+		})
+	})
+
+	Context("when updating elements on a list", func() {
+		It("should create new versions of the list when using ints", func() {
+			list := New(1, 2, 3, 4)
+			Expect(list.Values(0)).Should(Equal([]interface{}{1, 2, 3, 4}))
+
+			list.Update(2, 5)
+			Expect(list.Values(1)).Should(Equal([]interface{}{1, 2, 5, 4}))
+		})
+
+		It("should create new versions of the list when using strings", func() {
+			list := New("hi", "hello", "bye")
+			Expect(list.Values(0)).Should(Equal([]interface{}{"hi", "hello", "bye"}))
+
+			list.Update(2, "good")
+			Expect(list.Values(1)).Should(Equal([]interface{}{"hi", "hello", "good"}))
+		})
+	})
+
+	Context("when removing elements from a list", func() {
+		It("should create new versions of the list when using ints", func() {
+			list := New(1, 2, 3, 4)
+			Expect(list.Values(0)).Should(Equal([]interface{}{1, 2, 3, 4}))
+
+			list.Remove(2)
+			Expect(list.Values(1)).Should(Equal([]interface{}{1, 2, 4}))
+		})
+
+		It("should create new versions of the list when using strings", func() {
+			list := New("hi", "hello", "bye")
+			Expect(list.Values(0)).Should(Equal([]interface{}{"hi", "hello", "bye"}))
+
+			list.Remove(1)
+			Expect(list.Values(1)).Should(Equal([]interface{}{"hi", "bye"}))
+		})
+	})
+})
